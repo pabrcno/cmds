3e4166fd603ad435ea454d0b9dcb55096d08f6b2
pabrcno <pabrcn@gmail.com>
2023-05-16 18:43:57 -0300

    first commit

---
+++ install-initial-react-libs.cmd
@@ -0,0 +1,3 @@
+@echo off
+yarn add react-hook-form @tanstack/react-query @tanstack/react-query-devtools notistack axios lodash @hookform/resolvers zod
+yarn add -D @types/react-hook-form @types/tanstack__react-query @types/tanstack__react-query-devtools @types/notistack @types/axios @types/lodash @types/hookform__resolvers @types/zod react-test-renderer jest
---
+++ install-mui.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+echo Installing Material-UI dependencies...
+
+yarn add @mui/material @emotion/react @emotion/styled @mui/icons-material styled-components @types/styled-components
+
+echo Installation completed.
